d36db4e836453753718a975ab2d3f956ea83bc50
Clesia Munhembye <munhembyesergio94clesia@gmail.com>
2025-05-17 11:30:19 +0200

    Melhoramento da interface do projecto

--- src/main/java/org/cleu/gestaoDeConteudo/controller/UsuarioController.java
+++ src/main/java/org/cleu/gestaoDeConteudo/controller/UsuarioController.java
@@ -4,12 +4,9 @@ import org.cleu.gestaoDeConteudo.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import ch.qos.logback.core.model.Model;
 
 import org.cleu.gestaoDeConteudo.repository.UsuarioRepository;
 import jakarta.validation.Valid;
--- src/main/java/org/cleu/gestaoDeConteudo/model/Midia.java
+++ src/main/java/org/cleu/gestaoDeConteudo/model/Midia.java
@@ -3,7 +3,6 @@ package org.cleu.gestaoDeConteudo.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-import org.hibernate.annotations.Check;
 
 @Getter
 @Setter
